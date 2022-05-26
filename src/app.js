9557a9b1d97803141f135c3095c31f33b5a0fe55
Nithin Kumar BJ <nithinkumarbj@gmail.com>
2022-05-26 15:28:46 +0530

    setup app for hiroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forcast=require('./utils/forecast')
 const { error } = require('console')
 
 const app=express()
+const port=process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath= path.join(__dirname,'../public')
@@ -126,6 +127,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port '+ port)
 })
